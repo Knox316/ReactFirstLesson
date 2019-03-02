dddfa65e38f5d7700694852cd95baf91c4df381f
Knox316 <fernandocorreia316@gmail.com>
2019-03-02 00:38:42 +0000

    understanding props and prop children and styling components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,15 +5,80 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Fernando', age: 30
+      },
+      {
+        name: 'Correia', age: 30
+      },
+      {
+        name: 'Sandra', age: 29
+      }
+    ],
+    otherState: 'some other state'
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        {
+          name: newName, age: 58
+        },
+        {
+          name: 'Fernando Correia', age: 30
+        },
+        {
+          name: 'Sandra Correia', age: 23
+        }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {
+          name: 'Antonio', age: 58
+        },
+        {
+          name: event.target.value, age: 30
+        },
+        {
+          name: 'Sandra Correia', age: 23
+        }
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid gray',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return ( 
       <div className = "App" >
       <h1> React app </h1>  
       <p> This is working! </p>
-      <Person name='Fernando' age='30'/>
-      <Person name ='Correia' age='30'>My Hobbies: Gym</Person>
-      <Person name='Sandra' age='23'/>
-      </div >
+      <button
+        style={style} 
+        onClick={() => this.switchNameHandler('FERNANDO!')}>Switch Name</button>
+      <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+      <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
+        click={this.switchNameHandler.bind(this, 'Antonio')}
+        changed={this.nameChangedHandler}>My Hobbies: Gym</Person>
+      <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
+      </div>
     );
   }
 }
